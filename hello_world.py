9a8bf83ad2f4dd98a317bc16bedc5f754f5ae3f8
Imran Bhatti <imrany23@blueyonder.co.uk>
2024-02-29 23:30:47 +0000

    loading changes

--- hello_world.py
+++ hello_world.py
@@ -5,11 +5,3 @@ Program to print "Hello World!"
 print("Git is awesome! ")
 message = input ("Please enter a message to be printed")
 print(message)
-
-'''
-Program to print "Hello World!"
-'''
-
-print("Git is awesome! ")
-message = input ("Please enter a message to be printed")
-print(message)
